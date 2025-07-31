c22af88a5e85d08f922c1370a07d5da292b03b6b
meadowjp <136203773+meadowjp@users.noreply.github.com>
2025-07-31 14:10:49 -0400

    Update calculator.py
    Added functions

--- calculator.py
+++ calculator.py
@@ -4,9 +4,33 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a+b
+    
+def sub(a, b):
+    return a-b
+    
+def mul(a,b):
+    return a*b
+    
+def div(a, b):
+    try:
+        b/a
+    except ZeroDivisionError:
+        return "Division error"
+    return b/a
+    
+def log(a,b):
+    try:
+        math.log(a,b)
+    except ValueError:
+        return "Value error"
+    return math.log(a,b)
+    
+def exp(a,b):
+    return math.exp(a*b)
 
 
 #ANYTHING
